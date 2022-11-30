86f9463cc171d0aea547741204126245734942d5
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ ideator/app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="w-full flex-center">
+      <h1 className="head_text text-center">
+        Discover and share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p>
+        A modern tool to share promps.A modern tool to share promps. A modern
+        tool to share promps. A modern tool to share promps.{" "}
+      </p>
+    </section>
+  );
+}
